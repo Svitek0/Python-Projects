1e655f1a75f09c73184f40a3afddef720550a5bb
D32\zak <spseplzen@spseplzen.cz>
2025-10-03 12:24:07 +0200

    treti commit

--- main.py
+++ main.py
@@ -20,3 +20,27 @@ print(type(desetine_cislo))
 vysledek = 5/7
 print(vysledek)
 print(type(vysledek))
+
+x = "text"
+print(x, " - první x")
+x = 5
+print(x, " - druhé x")
+
+#cisla
+
+a = 5
+b = 7
+c = a + b
+d = a-b
+e = a * b
+f = a / b
+g = a**2
+h = a ** (1/2)
+i = b % a
+print(c, d, e, f, g, i)
+
+x = 5.8
+print(round(x))
+print(int(x))
+
+y = 5 + 5 * 2
