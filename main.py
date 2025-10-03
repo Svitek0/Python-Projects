c0d8184c31e0878984ec39afbbb8c5cc1e09ae72
D32\zak <spseplzen@spseplzen.cz>
2025-10-03 12:08:57 +0200

    druhy commit

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 print("Hello World")
-print("Ahoj")
+
+a = "ahoj"
+b = a
+
+print(b) # komentář
+
+samopipisne_nazvy_promenych = "ahoj"
+
+print(type(samopipisne_nazvy_promenych))
+
+cislo = 9
+print(cislo)
+print(type(cislo))
+
+desetine_cislo = 7.356
+print(desetine_cislo)
+print(type(desetine_cislo))
+
+vysledek = 5/7
+print(vysledek)
+print(type(vysledek))
