f888e8bd46ec9ffa4c2c2d5210fdc2c9deccd845
D32\zak <spseplzen@spseplzen.cz>
2025-10-17 12:04:26 +0200

    commit

---
+++ while loops.py
@@ -0,0 +1,32 @@
+# for i in range(11):
+#     if((i%2)==0):
+#         print(i, "číslo je sudé")
+#     else:
+#         print(i,  "čislo je liché")
+#
+# a = 0
+#
+# while (a==0):
+#     x = input("napiš číslo : ")
+#     try:
+#         a = int(x)
+#     except:
+#         a = 0
+
+while (True):
+    x = input("y / n : ")
+    if (x == "y"):
+        while (True):
+            x = input("napiš číslo : ")
+            try:
+                a = int(x)
+            except:
+                a = 0
+            if (a == 5):
+                print("uhodl jsi číslo")
+                break
+    else:
+        break
+    y = input("chcete začít znovu ? (y/n)")
+    if y == "n":
+        break
