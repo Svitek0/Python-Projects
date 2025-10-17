b682e35e6d36646583ede9489fc128c13fdb12a0
D32\zak <spseplzen@spseplzen.cz>
2025-10-17 12:23:54 +0200

    commit

--- příklad.py
+++ příklad.py
@@ -8,6 +8,11 @@
 #     if((i%2) ==0):
 #         print(i)
 
-for i in range(5, 12):
-    for j in range(12, 5, -1):
-        print(i + j)
+# for i in range(5, 12):
+#     for j in range(12, 5, -1):
+#         print(i + j)
+
+soucet = 0
+for i in range(5,12):
+    soucet = soucet + 1
+    print(soucet)
