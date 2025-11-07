b161e946f8e47f9805809ca60961cd6a8d2ba607
D32\zak <spseplzen@spseplzen.cz>
2025-11-07 11:25:49 +0100

    commit

---
+++ 10012.py
@@ -0,0 +1,10 @@
+promena = "Ahoj Karle, jak se máš?"
+
+for i in range(len(promena)):
+    print(promena[i])
+
+for i in range(len(promena)):
+    print(promena[-1-i])
+
+for i in range(len(promena)-1, -1, -1):
+    print(promena[i])
