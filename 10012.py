f08acd084702bfed6ff436456ffe561ef118bdc7
D32\zak <spseplzen@spseplzen.cz>
2025-11-07 12:02:08 +0100

    commit

--- 10012.py
+++ 10012.py
@@ -16,6 +16,22 @@ for i in range(len(promena)):
 
 print("*************")
 
-for i in range(len(promena)-1):
-    print(promena[i:2])
+for i in range(len(promena)-2):
+    print(promena[i:i+3])
 
+print("*************")
+
+#vipiste vždy vedle sebe prvni a posledni pismenko
+#pak dtuhy a predposlední ... končíme v polivině textu
+
+for i in range(int(len(promena)/2)+1):
+    print(promena[i], promena[-i-1])
+
+
+print("*************")
+print(promena)
+a = "   adsakosnd"
+print(a.strip())
+
+b = "4"
+print(b.zfill(5))
