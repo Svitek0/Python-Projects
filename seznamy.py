471fbf5558d2c2335e9462570d67234beb3191c3
D32\zak <spseplzen@spseplzen.cz>
2025-11-07 12:13:56 +0100

    commit

---
+++ seznamy.py
@@ -0,0 +1,19 @@
+promena = [1, 2, 3, 4, 5]
+
+print(promena)
+
+p2 = [1, "abc", 5.5, True, [1,2,"dsa"]]
+print(p2)
+print(p2[2])
+print(type(p2[4][1]))
+
+p2 = [1, "abc", 5.5, True, [1,2,"dsa"]]
+print(p2)
+print(p2[1][1])
+print(type(p2[4][1]))
+
+p2 = [1, "abc", 5.5, True, [1,2,"dsa"]]
+print(p2)
+print(p2[:3])
+
+x = [1, 2, 8, 4, 9, 10, 7, 3]
