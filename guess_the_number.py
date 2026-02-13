56e9d6fdea8affce0a9bf174497e85753fb9ff83
D32\zak <spseplzen@spseplzen.cz>
2026-02-13 11:23:06 +0100

    commit

--- guess_the_number.py
+++ guess_the_number.py
@@ -4,14 +4,16 @@ import numpy as np
 
 cislo = np.random.randint(1,21)
 print(cislo)
+pocet = 0
 
 while(True):
     pokus = input("zadejte číslo od 1 do 20 : ")
-
+    pocet +=1
     try:
         pokusINT = int(pokus)
         if (pokusINT == cislo):
             print("Správně!")
+            print("Počet pokusů :" + str(pocet))
             break
         elif (pokusINT > cislo):
             print("Hledané číslo je MENŠÍ.")
