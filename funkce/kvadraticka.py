38baa9483f1b7404c8be927c9d10f042071c553d
D32\zak <spseplzen@spseplzen.cz>
2026-02-20 11:22:49 +0100

    commit

--- funkce/kvadraticka.py
+++ funkce/kvadraticka.py
@@ -3,11 +3,18 @@ def kvad(a,b,c):
     kva1 = (-b + pow(disk,(1/2)) ) / (2*a)
     kva2 = (-b - pow(disk,(1/2)) ) / (2*a)
     if (disk < 0 ):
-        return print("diskriminant je záporný")
+        return [] #print("diskriminant je záporný")
     elif(disk == 0):
         return kva1
     else:
         return [kva1, kva2]
 
+def kvadratic_test():
+    results=[]
+    for i in range(5):
+        results.append(kvad(1,i,1))
+    return results
 
-print(kvad(1,3,1))
+vysledky = kvadratic_test()
+for vysledek in vysledky:
+    print(vysledek)
