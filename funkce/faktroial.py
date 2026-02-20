c6c0dfa7eb1572b6e74a08feb9ca54f6df59974b
D32\zak <spseplzen@spseplzen.cz>
2026-02-20 11:54:35 +0100

    commit

---
+++ funkce/faktroial.py
@@ -0,0 +1,11 @@
+from math import factorial
+
+
+def faktroial(n):
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+for i in range(10):
+    print(faktroial(i))
--- funkce/funkce_funkci.py
+++ funkce/funkce_funkci.py
@@ -9,5 +9,13 @@ def vynasob_soucet_a_rozdil(a,b,c,d):
     rozdil = odecti(c,d)
     return soucet * rozdil
 
+def kalkulacka(a,b, funkce):
+    return funkce(a,b)
+
 vysledek = vynasob_soucet_a_rozdil(1,2,5,3)
 print("vysledek (a+b) * (a-b) = " + str(vysledek))
+
+vysledek2 = kalkulacka(2,3,secti)
+vysledek3 = kalkulacka(2,3,odecti)
+print(vysledek2)
+print(vysledek3)
