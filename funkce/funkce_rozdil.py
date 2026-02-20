679b1bc107d979c98f5aace047dc6d1b57da4810
D32\zak <spseplzen@spseplzen.cz>
2026-02-20 11:33:02 +0100

    commit

---
+++ funkce/funkce_rozdil.py
@@ -0,0 +1,14 @@
+def secti(a,b):
+    return a+b
+
+def vypis_soucet(a,b):
+    print(a+b)
+
+def secti_ret_print(a,b):
+    return print(a+b)
+
+print(secti(2,3))
+print("**********")
+print(vypis_soucet(3,2))
+print("**********")
+print(secti_ret_print(2,3))
