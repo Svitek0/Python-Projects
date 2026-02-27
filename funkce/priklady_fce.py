a283777c4acd0d6aab419318d0dc2962efbda4a7
D32\zak <spseplzen@spseplzen.cz>
2026-02-27 12:25:17 +0100

    commit

--- funkce/priklady_fce.py
+++ funkce/priklady_fce.py
@@ -1,16 +1,36 @@
-def druhynejvetsi(list):
-    max = 0
-    druh=0
-    for i in range(len(list)):
-        if(list[i] > max):
+def druhy_nejvetsi(vlist):
+    if vlist[0]> vlist[1]:
+        max = vlist[0]
+        druh = vlist[1]
+    else:
+        max = vlist[1]
+        druh = vlist[0]
+    for i in range(len(vlist)):
+        if(vlist[i] > max):
             druh = max
-            max = list[i]
-        if(list[i]>druh and list[i] < max):
-             druh = list[i]
+            max = vlist[i]
+        if(vlist[i]>druh and vlist[i] < max):
+             druh = vlist[i]
 
     return druh
 
 
-a = [10,2,5,40,30,12,20,34]
-x = druhynejvetsi(a)
+a = [10,2,5,40,30,12,20,34,-9,0,1]
+x = druhy_nejvetsi(a)
 print(x)
+
+
+
+
+def je_prvocislo(a):
+    if a <2:
+        return False
+
+    for i in range(2,a):
+        if a%i==0:
+            return False
+
+    return True
+
+cislo = 21
+print(je_prvocislo(cislo))
