400c875a1a0b949d8e3fd36bf84245891565e6c5
D32\zak <spseplzen@spseplzen.cz>
2026-04-24 12:25:08 +0200

    commit

--- příklady/příklad1.py
+++ příklady/příklad1.py
@@ -1,9 +1,43 @@
 import random
+from traceback import print_tb
 
 x= random.randint(10,20)
 
 def hod_k6():
+    """
+    simuluje hod kostkou
+    :return:
+    int: rand <1;6>
+    """
     hod = random.randint(1, 6)
     return  hod
 
 print(hod_k6())
+print("*******************")
+
+def pocet_hodu1():
+    pocet = 0
+    while (True):
+        pocet+=1
+        hod = hod_k6()
+        if(hod == 6):
+            break
+    return pocet
+
+print(pocet_hodu1())
+
+def hazeni_n_krat(n):
+    cetnost = [0]*20
+    for i in range(n):
+        x = pocet_hodu1()
+        if x > 20:
+            x= 20
+        cetnost[x-1] += 1
+
+    return cetnost
+
+random.seed(5)
+print(hazeni_n_krat(10000))
+
+
+
